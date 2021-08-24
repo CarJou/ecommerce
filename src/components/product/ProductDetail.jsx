4011b0bfaa8b4542be2e7b4601962a8939bf2cf4
C:/Program Files/Git/CarJou <carlajoumier@gmail.com>
2021-08-23 22:07:32 -0300

    add change

--- src/components/product/ProductDetail.jsx
+++ src/components/product/ProductDetail.jsx
@@ -1,7 +1,7 @@
-import React, { useContext } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 
-const ProductDetail = ({ match }) => {
+const ProductDetail = () => {
   const { id } = useParams();
 
   return <>Product id: {id}</>;
